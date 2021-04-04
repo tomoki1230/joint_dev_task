2df13e684ec1355610fb6498deadcb2f44b53aa2
tomoki <tomoki_1230@yahoo.co.jp>
2021-04-05 00:24:08 +0900

    Q1書き換え、Q4〜6を回答

--- task.rb
+++ task.rb
@@ -5,7 +5,7 @@ def q1
   names = ["田中", "佐藤", "佐々木", "高橋"]
 
   # 以下に回答を記載
-  names.push("斉藤")
+  names << "斉藤"
   puts names
 end
 
@@ -29,7 +29,7 @@ def q4
   sports = ["サッカー", "フットサル", nil, "野球", "バスケ", nil, "バレー"]
 
   # 以下に回答を記載
-
+  sports.compact!
   # 以下は変更しないで下さい
   p sports
 end
@@ -39,14 +39,18 @@ def q5
   array2 = [1, 5, 8, 10]
 
   # 以下に回答を記載
-
+  p array1.empty?
+  p array2.empty?
 end
 
 def q6
   numbers1 = [1, 2, 3, 4, 5]
 
   # 以下に回答を記載
-
+  numbers2 = numbers1.map do |number|
+    number * 10
+  end
+  p numbers2
 end
 
 def q7
